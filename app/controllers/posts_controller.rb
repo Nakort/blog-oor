49a6d9c1eb0de783813ce9556537063e4615ec83
Nakort <nakort@gmail.com>
2012-06-07 15:36:08 -0430

    Added create action for posts, removed blog#index dummy entries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,11 @@ class PostsController < ApplicationController
   def new
     @post = @blow.new_post
   end
+
+  def create
+    @post = @blog.new_post(params[:post])
+    @post.publish
+    redirect_to root_path, notice: "Post added!"
+  end
+
 end
